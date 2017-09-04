14a310647ccbb080a12c3d54731186e17981272c
Mohammad Javad Mehdizadeh <mehdizadeh@rooyekhat.co>
2017-09-05 02:14:40 +0430

    TextInput default style

--- src/components/BaseUI/TextInput/index.js
+++ src/components/BaseUI/TextInput/index.js
@@ -1,3 +1,19 @@
+import React from 'react';
 import {TextInput} from 'react-native';
+import styles from './index.style';
 
-export default TextInput;
+export default (props) => {
+  const defaultProps = {
+    ...props,
+    ...{
+      underlineColorAndroid: 'transparent',
+      placeholderTextColor: '#adadad',
+      style: {
+        ...styles.textInput,
+        ...(props.style || {}),
+      },
+    },
+  };
+
+  return (<TextInput {...defaultProps}/>);
+};
---
+++ src/components/BaseUI/TextInput/index.style.js
@@ -0,0 +1,13 @@
+export default {
+  textInput: {
+    height: 45,
+    borderWidth: 1,
+    borderColor: '#e6e6e6',
+    borderRadius: 4,
+    backgroundColor: '#fcfcfc',
+    fontSize: 13,
+    color: '#252525',
+    paddingLeft: 15,
+    paddingRight: 15,
+  },
+};
