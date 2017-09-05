af58c650783d9665e5c452869e4af24859102cea
Mohammad Javad Mehdizadeh <mehdizadeh@rooyekhat.co>
2017-09-06 03:40:32 +0430

    Fix Heartbeat

--- src/modules/Handler/Heartbeat.js
+++ src/modules/Handler/Heartbeat.js
@@ -9,7 +9,7 @@ import Api from '../Api/index';
 export default class Heartbeat extends Base {
   handle() {
     const heartBeat = new Heartbeat();
-    Api.invoke(HEARTBEAT).catch(() => {
+    Api.invoke(HEARTBEAT, heartBeat).catch(() => {
     });
   }
 }
