3ff2fbe2fb77a70a432fef01521ce995ab811f21
Mohammad Javad Mehdizadeh <mehdizadeh@rooyekhat.co>
2017-09-25 09:34:24 +0330

    Heartbeat fix

--- src/modules/Handler/Heartbeat.js
+++ src/modules/Handler/Heartbeat.js
@@ -1,6 +1,9 @@
 import Base from './Base';
 import {HEARTBEAT} from '../../constants/methods/index';
 import Api from '../Api/index';
+import {Heartbeat as ProtoHeartbeat} from '../Proto/index';
+import ServerError from '../Error/ServerError';
+import {ERROR_TIMEOUT} from '../Api/errors/index';
 
 /**
  * @property {ProtoHeartbeat} _request
@@ -8,8 +11,12 @@ import Api from '../Api/index';
  */
 export default class Heartbeat extends Base {
   handle() {
-    const heartBeat = new Heartbeat();
-    Api.invoke(HEARTBEAT, heartBeat).catch(() => {
+    const heartBeat = new ProtoHeartbeat();
+    Api.invoke(HEARTBEAT, heartBeat).catch((e) => {
+      if (e instanceof ServerError && e.errorResponse.getMajorCode() === ERROR_TIMEOUT) {
+        return Promise.resolve();
+      }
+      return Promise.reject(e);
     });
   }
 }
