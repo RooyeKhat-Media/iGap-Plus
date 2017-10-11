8fb292617a8bd9ecea02955dc2a0d14d59132069
Mohammad Javad Mehdizadeh <mehdizadeh@rooyekhat.co>
2017-10-11 15:28:42 +0330

    Gif Message attachment Box

--- src/components/Unit/RoomMessage/MessageBox/Gif.js
+++ src/components/Unit/RoomMessage/MessageBox/Gif.js
@@ -1,13 +1,61 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {ActivityIndicator, Image, StyleSheet, TouchableOpacity, View} from 'react-native';
+import Text from './Text';
+import {min} from 'lodash';
+import {Icon, ProgressBar} from '../../../BaseUI';
+import Device from '../../../../modules/Responsive/Device';
+import {dimensionCalculate} from '../../../../utils/core';
 
-export default ({message, attachment}) => (
-  <View style={styles.container}><Text>{message}</Text></View>
-);
+const {width, height} = Device.dimensions.window;
+const boxWidth = min([250, (0.7 * width)]);
+const boxHeight = min([500, (0.8 * height)]);
+
+export default ({message, attachment}) => {
+
+  const {width, height} = dimensionCalculate(attachment.getWidth(), attachment.getHeight(), boxWidth, boxHeight);
+  const uri = 'file://' + (attachment.isCompleted ? attachment.downloadFile.uri : attachment.smallThumbnailUri);
+
+  return (<View style={[styles.container, {width}]}>
+
+    <TouchableOpacity onPress={attachment.togglePress} activeOpacity={0.9}>
+      <Image blurRadius={attachment.isCompleted ? 0 : 0.5}
+        source={{uri: uri}}
+        style={[styles.imageWrap, {width, height}]}>
+
+        {attachment.isCompleted && (
+          <View style={styles.downloadBtn}><Icon name="gif" size={30} color="#fafafa"/></View>)}
+
+        {attachment.isPending && (<ActivityIndicator size="large"/>)}
+        {attachment.isProcessing && ((<ProgressBar
+          style={[styles.progressStyle]}
+          width={width - 10}
+          initialProgress={attachment.downloadFile.progress}
+          progress={attachment.downloadFile.progress}/>))}
+        {attachment.isPaused && (
+          <View style={styles.downloadBtn}><Icon name="file-download" size={30} color="#fafafa"/></View>)}
+      </Image>
+    </TouchableOpacity>
+
+    {message ? (<Text message={message}/>) : null}
+  </View>);
+};
 
 const styles = StyleSheet.create({
   container: {},
-  message: {
-    color: '#202020',
+  imageWrap: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  downloadBtn: {
+    backgroundColor: 'rgba(64, 64, 64, 0.6)',
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  progressStyle: {
+    marginBottom: 5,
   },
 });
