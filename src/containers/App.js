394cc5b27d8004b8eb2609090fa5b270593ea3d0
Mohammad Javad Mehdizadeh <mehdizadeh@rooyekhat.co>
2017-10-14 15:29:51 +0330

    Change StatusBar backgroundColor

--- src/containers/App.js
+++ src/containers/App.js
@@ -74,7 +74,7 @@ class App extends Component {
       <ThemeProvider uiTheme={appTheme}>
         <View style={{flex: 1}}>
           <View style={{flex: 1, zIndex: 2}}>
-            <StatusBar backgroundColor="#f0f0f0" barStyle="dark-content"/>
+            <StatusBar backgroundColor="#e5e5e5" barStyle="dark-content"/>
             <AppNavigator navigation={addNavigationHelpers({dispatch, state: nav})}/>
           </View>
           <AppModal/>
