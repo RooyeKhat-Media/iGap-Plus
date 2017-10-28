5a43d0b7a6a73803bf9d68aa68ae1b6f6c39166b
Asghar Amereii <amereii@rooyekhat.co>
2017-10-28 14:17:24 +0330

    Collector module

---
+++ src/modules/Collector/index.js
@@ -0,0 +1,51 @@
+/**
+ * @flow
+ */
+
+import 'es6-map/implement';
+
+/**
+ *
+ * @param {function} collectorCallback
+ * @param {number} collectorDelay
+ * @param {number} collectQueueSize
+ * @param {boolean} arrayStorage
+ * @return {{collect: (function(*, (string|number)))}}
+ */
+export default function(collectorCallback, collectorDelay = 500, collectQueueSize = 100, arrayStorage = false) {
+
+  let collectorStorage = arrayStorage ? [] : new Map();
+  let collectQueueTimer = null;
+
+  const flush = () => {
+    collectQueueTimer = null;
+    const collected = collectorStorage;
+    collectorStorage = arrayStorage ? [] : new Map();
+    collectorCallback(collected);
+  };
+
+
+  return {
+    /**
+     * Collect data
+     * @param {*} data
+     * @param {string|number} id Omitted for array storage
+     */
+    collect: (data, id) => {
+      if (arrayStorage) {
+        collectorStorage.push(data);
+      } else {
+        collectorStorage.set(id, data);
+      }
+
+      const capacity = arrayStorage ? collectorStorage.length : collectorStorage.size;
+
+      if (collectQueueSize <= capacity) {
+        clearTimeout(collectQueueTimer);
+        flush();
+      } else if (collectQueueTimer === null) {
+        collectQueueTimer = setTimeout(flush, collectorDelay);
+      }
+    },
+  };
+}
