9c631d6a075a19acbffc7124bafbbeaedfe762ff
Mehdi Nejati <nejati@rooyekhat.co>
2017-10-28 16:07:48 +0330

    Audio Box Done

--- src/utils/filters.js
+++ src/utils/filters.js
@@ -9,3 +9,12 @@ export function convertBytes(x) {
   }
   return (n.toFixed(n >= 10 || l < 1 ? 0 : 1) + ' ' + units[l]);
 }
+
+export function convertSecendToTime(secend) {
+  const mins = Math.floor(secend / 60);
+  const secs = Math.round(secend - mins * 60);
+  const hrs = Math.floor(secend / 3600);
+  return (hrs > 0 ? (hrs > 9 ? hrs + ':' : '0' + hrs + ':') : '') + (mins > 9 ? mins : '0' + mins) + ':' + (secs > 9 ? secs : '0' + secs);
+}
+
+
