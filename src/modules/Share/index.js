282ae34c8ab58d9fd5ea582399fc2ca37485f33d
Asghar Amereii <amereii@rooyekhat.co>
2017-10-30 14:22:47 +0330

    Share module

---
+++ src/modules/Share/index.js
@@ -0,0 +1,28 @@
+import ReactNativeShare from 'react-native-share';
+
+export default class Share {
+  /**
+   * @return {boolean}
+   */
+  static get isSupported() {
+    return true;
+  }
+
+  /**
+   * @param {string} url
+   * @param {string} mimeType
+   * @param {string} message
+   * @return Promise
+   */
+  static open(url, mimeType, message = '') {
+    if (!Share.isSupported) {
+      return Promise.reject('Share is not supported on this platform');
+    }
+
+    return ReactNativeShare.open({
+      url,
+      type : mimeType || 'application/octet-stream',
+      message,
+    });
+  }
+}
