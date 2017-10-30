c0e8e468f31892eacbfc68c305329c78b3e9506a
Mehdi Nejati <nejati@rooyekhat.co>
2017-10-30 17:49:08 +0330

    Group Box Done

--- src/components/Unit/RoomMessage/GroupBox.js
+++ src/components/Unit/RoomMessage/GroupBox.js
@@ -3,16 +3,17 @@ import {StyleSheet, Text, View} from 'react-native';
 import MessageBox from './MessageBox/index';
 import Avatar from '../../../containers/Unit/Avatar';
 import AddonTime from './MessageBox/AddonTime';
+import {white} from '../../../themes/default/index';
 
 export default ({message}) => (
   <View style={styles.container}>
     <View style={styles.avatarWrap}>
-      {message.authorUserObject ? (<Avatar userId={message.authorUserObject.id} size={35}/>) : null}
+      {message.authorUserObject ? (<Avatar userId={message.authorUserObject.id} size={40}/>) : null}
     </View>
+    <View style={styles.triangle}/>
     <View style={styles.messageBox}>
-      {message.authorUserObject ? (<Text style={[styles.author, {color: message.authorUserObject.color}]}>
-        {message.authorUserObject.displayName}
-      </Text>) : null}
+      {message.authorUserObject ? (<Text
+        style={[styles.author, {color: message.authorUserObject.color}]}>{message.authorUserObject.displayName}</Text>) : null}
       <MessageBox message={message}/>
       <View style={styles.footerWrap}>
         <AddonTime createTime={message.createTime}/>
@@ -25,21 +26,40 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'row',
+    marginLeft: 5,
     justifyContent: 'flex-start',
   },
   avatarWrap: {
-    width: 38,
-    marginLeft: 4,
+    width: 40,
+    height: 40,
+    alignSelf: 'flex-end',
+    marginRight: -3,
   },
   messageBox: {
-    backgroundColor: '#fff',
-    borderRadius: 5,
+    backgroundColor: white,
+    borderRadius: 15,
     padding: 5,
     minWidth: 30,
     margin: 5,
     maxWidth: 260,
     flexDirection: 'column',
     justifyContent: 'space-between',
+  },
+  triangle: {
+    width: 0,
+    height: 0,
+    backgroundColor: 'rgba(0,0,0,0)',
+    alignSelf: 'flex-end',
+    marginRight: -14,
+    borderLeftWidth: 0,
+    borderRightWidth: 16,
+    borderTopWidth: 6,
+    borderBottomWidth: 5,
+    borderLeftColor: 'rgba(0,0,0,0)',
+    borderRightColor: white,
+    borderTopColor: 'rgba(0,0,0,0)',
+    borderBottomColor: 'rgba(0,0,0,0)',
+    transform: [{rotate: '-45deg'}],
   },
   author: {
     fontSize: 11,
