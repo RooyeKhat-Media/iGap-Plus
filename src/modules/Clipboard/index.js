2ec9962c2b1dc472674e1c44446a13600f09ff2f
Asghar Amereii <amereii@rooyekhat.co>
2017-11-01 10:31:59 +0330

    Clipboard module

---
+++ src/modules/Clipboard/index.js
@@ -0,0 +1,36 @@
+import {Clipboard as RNClipboard} from 'react-native';
+
+export default class Clipboard {
+  /**
+   * @return {boolean}
+   */
+  static get isSetSupported() {
+    return true;
+  }
+
+  /**
+   * @param {string} content
+   */
+  static setString(content) {
+    if (this.isSetSupported()) {
+      RNClipboard.setString(content);
+    }
+  }
+
+  /**
+   * @return {boolean}
+   */
+  static get isGetSupported() {
+    return true;
+  }
+
+  /**
+   * @param {string} content
+   */
+  static getString(content) {
+    if (!this.isGetSupported()) {
+      return Promise.reject('Getting content from Clipboard is not supported');
+    }
+    return RNClipboard.getString();
+  }
+}
