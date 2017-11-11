2369b20f3dd202cb5f4c15560ae0283403dbcc67
Mehdi Nejati <nejati@rooyekhat.co>
2017-11-11 16:48:01 +0330

    CallScreen Bug fix

--- src/screens/CallScreen.js
+++ src/screens/CallScreen.js
@@ -1,4 +1,4 @@
-import React, {PropTypes, Component} from 'react';
+import React, {Component} from 'react';
 import CallComponent from '../components/Call/index';
 
 class CallScreen extends Component {
