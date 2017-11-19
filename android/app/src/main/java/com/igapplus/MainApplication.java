b5ce8b3c467282e43619a6e0d357df5403d3bfb2
Asghar Amereii <amereii@rooyekhat.co>
2017-11-19 12:38:05 +0330

    Added react-native-sound

--- android/app/src/main/java/com/igapplus/MainApplication.java
+++ android/app/src/main/java/com/igapplus/MainApplication.java
@@ -3,6 +3,7 @@ package com.igapplus;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.zmxv.RNSound.RNSoundPackage;
 import com.reactlibrary.RNSoundRecorderPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.centaurwarchief.smslistener.SmsListenerPackage;
@@ -33,6 +34,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNSoundPackage(),
             new RNSoundRecorderPackage(),
             new RCTCameraPackage(),
             new SmsListenerPackage(),
