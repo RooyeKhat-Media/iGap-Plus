edf8f59846808d6dc760affbc562730289c03c1a
Asghar Amereii <amereii@rooyekhat.co>
2018-02-15 09:43:01 +0330

    Added react-native-incall-manager

--- android/app/src/main/java/com/igapplus/MainApplication.java
+++ android/app/src/main/java/com/igapplus/MainApplication.java
@@ -3,6 +3,7 @@ package com.igapplus;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.zxcpoiu.incallmanager.InCallManagerPackage;
 import com.shahenlibrary.RNVideoProcessingPackage;
 import com.rt2zz.reactnativecontacts.ReactNativeContacts;
 import com.airbnb.android.react.maps.MapsPackage;
@@ -39,6 +40,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new InCallManagerPackage(),
             new RNVideoProcessingPackage(),
             new ReactNativeContacts(),
             new MapsPackage(),
