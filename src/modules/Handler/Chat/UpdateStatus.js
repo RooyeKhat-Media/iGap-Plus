38c71a2144f876b2f5477b89f441a3d4456faece
Mohammad Javad Mehdizadeh <mehdizadeh@rooyekhat.co>
2018-04-03 16:02:45 +0330

    Update unreadCount

--- src/modules/Handler/Chat/UpdateStatus.js
+++ src/modules/Handler/Chat/UpdateStatus.js
@@ -2,11 +2,27 @@ import Base from '../Base';
 import Collector from '../../Collector';
 import store from '../../../configureStore';
 import {entitiesRoomMessageEdit} from '../../../actions/entities/roomMessages';
+import {getAuthorHash} from '../../../utils/app';
+import {entitiesRoomEdit} from '../../../actions/entities/rooms';
+import {Proto} from '../../Proto/index';
 
 const {collect} = Collector(
   (collected) => {
+    let seenMessages = {};
     for (let [messageId, payload] of collected.entries()) {
-      store.dispatch(entitiesRoomMessageEdit(messageId, payload));
+      store.dispatch(entitiesRoomMessageEdit(messageId, {
+        status: payload.status,
+        statusVersion: payload.statusVersion,
+      }));
+      seenMessages[payload.roomId] = seenMessages[payload.roomId] || (
+        payload.status === Proto.RoomMessageStatus.SEEN &&
+        payload.updaterAuthorHash === getAuthorHash()
+      );
+    }
+    for (const roomId in seenMessages) {
+      if (seenMessages[roomId]) {
+        store.dispatch(entitiesRoomEdit(roomId, {unreadCount: 0}));
+      }
     }
   },
   250
@@ -20,6 +36,8 @@ export default class UpdateStatus extends Base {
     collect({
       status: this._response.getStatus(),
       statusVersion: this._response.getStatusVersion().toString(),
+      roomId: this._response.getRoomId().toString(),
+      updaterAuthorHash: this._response.getUpdaterAuthorHash(),
     }, this._response.getMessageId().toString());
   }
 }
